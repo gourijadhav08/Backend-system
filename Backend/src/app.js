6a704f4eb22961c86f72102f5bbad6f5219acf75
gourijadhav08 <gourivamanjadhav06@gmail.com>
2026-07-09 00:48:05 +0530

    POST   /api/notes        → Create a new note GET    /api/notes        → Get all notes DELETE /api/notes/:id    → Delete a note by ID PATCH  /api/notes/:id    → Update a note's description

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,27 +7,23 @@ app.use(express.json())
 
 
 app.post('/api/notes',async(req,res)=>{
-     const{ title,description}=req.body
+     const { title, description, discrption } = req.body
 
 
-
-  const note = await notemodel.create({
-         title,description
-
-         
+     const notes = await notemodel.create({
+         title,
+         description: description || discrption
      })
 
      res.status(200).json({
         message:"note create successfully",
-         note
+         notes
      })
 })
 
 app.get("/api/notes",async(req,res)=>{
 
-
-    
-    const notes = await notemodel.find()
+    const notes  = await notemodel.find()
 
     res.status(200).json({
         message:"its get",
@@ -35,4 +31,33 @@ app.get("/api/notes",async(req,res)=>{
     })
 })
 
+app.delete('/api/notes/:id',async(req,res)=>{
+     const id = req.params.id
+
+     await notemodel.findByIdAndDelete(id)
+
+     res.status(200).json({
+        message:"done",
+        
+     })
+})
+
+app.patch('/api/notes/:id', async(req,res)=>{
+   const id = req.params.id
+
+   const {description} = req.body
+   await  notemodel.findByIdAndUpdate(id,{description})
+
+
+
+    res.status(200).json({
+        message:"its update"
+    })
+})
+
 module.exports = app
+
+
+
+//cd "C:\Users\GAURI\OneDrive\Documents\Backend project\Backend"
+//npm start
